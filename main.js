2d745cdf94baf92ff70fee6893b639983fe2a77c
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-29 15:08:18 -0300

    feat: Add inn listing to home page with data from api

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
 const app = Vue.createApp({
     data(){
         return{
+            searchText: '',
             innList: []
         }
     },
     methods:{
-        async getData(){
+        async getInnIndex(){
             let response = await fetch('http://localhost:3000/api/v1/inns');
             let data = await response.json();
             console.log(data);
+
+            this.innList = []
+
             data.forEach(item =>{
                 var inn = new Object();
+                var address = new Object();
 
+                inn.id = item.id;
                 inn.name = item.name;
+                inn.email = item.email;
+                inn.phone = item.phone;
+                inn.pets = item.pets;
+                inn.policies = item.policies;
+                inn.check_in_time = item.checkInTime;
+                inn.check_out_time = item.checkOutTime;
+                inn.pix = item.pix;
+                inn.cash = item.cash;
+                inn.credit = item.credit;
+                inn.debit = item.debit;
+
+                address.street = item.address.street;
+                address.number = item.address.number;
+                address.neighborhood = item.address.neighborhood;
+                address.city = item.address.city;
+                address.state = item.address.state;
+                address.cep = item.address.cep;
+
+                inn.address = address
+
+                this.innList.push(inn)
             })
+            console.log(this.innList);
         }
     }
 
