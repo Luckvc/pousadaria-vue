0064dde7b4803abf74420f1053ac7ed0f6e980f6
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-29 02:54:56 -0300

    wip: consuming inns api

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 const app = Vue.createApp({
+    data(){
+        return{
+            innList: []
+        }
+    },
+    methods:{
+        async getData(){
+            let response = await fetch('http://localhost:3000/api/v1/inns');
+            let data = await response.json();
+            console.log(data);
+            data.forEach(item =>{
+                var inn = new Object();
+
+                inn.name = item.name;
+            })
+        }
+    }
 
 })
 
